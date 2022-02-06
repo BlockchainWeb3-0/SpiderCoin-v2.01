39fe411c8f21412a8e61cb684fc5055b16d594ad
imysh578 <imysh578@gmail.com>
2022-02-06 17:08:41 +0900

    edit routerTest

--- server/server_http/__test__/transactionRouter.test.ts
+++ server/server_http/__test__/transactionRouter.test.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import UnspentTxOutput from "../../blockchain/transaction/unspentTxOutput";
+import Transaction from "../../blockchain/transaction/transaction";
 import Wallet from "../../blockchain/wallet/wallet";
 
 describe("Rotuers test", () => {
@@ -18,26 +18,33 @@ describe("Rotuers test", () => {
 		await axios.request(params);
 	});
 	beforeEach(() => {
-		receiverAddress =
-			"04d2d156b54c47d0ad7acca55ad5e484eb0191a6d783e7473037d4d9f4af66455ee937826090e71dbbacc0a7e7540d2850f92812bce1f87180cac3900541794274";
+		receiverAddress = Wallet.generatePrivatePublicKeys().publicKey;
 		senderAddress = Wallet.getPulicKeyFromWallet();
 		privateKey = Wallet.getPrivateKeyFromWallet();
 		sendingAmount = 10;
 	});
 
-	// * get specific address's utxo list from whole utxo list
-	describe("utxos Router test", () => {
-		test("GET : /mine/:address", async () => {
+	// * create new transaction 
+	describe("transaction router test", () => {
+		test("Post: /transaction/create", async () => {
 			const params: object = {
-				method: "get",
+				method: "post",
 				baseURL: "http://localhost:3001",
-				url: `/utxos/mine/${senderAddress}`,
+				url: "/transaction/create",
+				data: {
+					receiverAddress,
+					sendingAmount,
+					senderAddress,
+					privateKey,
+				},
 			};
 			const result = await axios.request(params);
-			const myUtxoList: UnspentTxOutput[] = result.data;
-
-			for (let i = 0; i < myUtxoList.length; i++) {
-				expect(myUtxoList[i].address).toBe(senderAddress);
+			const tx: Transaction = result.data;
+			if (tx === null) {
+				expect(tx).toBe(null);
+			} else {
+				expect(tx.txOuts[0].address).toBe(receiverAddress);
+				expect(tx.txOuts[0].amount).toBe(10);
 			}
 		});
 	});
--- server/server_http/__test__/utxosRouter.test.ts
+++ server/server_http/__test__/utxosRouter.test.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Transaction from "../../blockchain/transaction/transaction";
+import UnspentTxOutput from "../../blockchain/transaction/unspentTxOutput";
 import Wallet from "../../blockchain/wallet/wallet";
 
 describe("Rotuers test", () => {
@@ -18,34 +18,25 @@ describe("Rotuers test", () => {
 		await axios.request(params);
 	});
 	beforeEach(() => {
-		receiverAddress =
-			"04d2d156b54c47d0ad7acca55ad5e484eb0191a6d783e7473037d4d9f4af66455ee937826090e71dbbacc0a7e7540d2850f92812bce1f87180cac3900541794274";
+		receiverAddress = Wallet.generatePrivatePublicKeys().publicKey;
 		senderAddress = Wallet.getPulicKeyFromWallet();
 		privateKey = Wallet.getPrivateKeyFromWallet();
 		sendingAmount = 10;
 	});
 
-	// * create new transaction 
-	describe("transaction router test", () => {
-		test("Post: /transaction/create", async () => {
+	// * get specific address's utxo list from whole utxo list
+	describe("utxos Router test", () => {
+		test("GET : /mine/:address", async () => {
 			const params: object = {
-				method: "post",
+				method: "get",
 				baseURL: "http://localhost:3001",
-				url: "/transaction/create",
-				data: {
-					receiverAddress,
-					sendingAmount,
-					senderAddress,
-					privateKey,
-				},
+				url: `/utxos/mine/${senderAddress}`,
 			};
 			const result = await axios.request(params);
-			const tx: Transaction = result.data;
-			if (tx === null) {
-				expect(tx).toBe(null);
-			} else {
-				expect(tx.txOuts[0].address).toBe(receiverAddress);
-				expect(tx.txOuts[0].amount).toBe(10);
+			const myUtxoList: UnspentTxOutput[] = result.data;
+
+			for (let i = 0; i < myUtxoList.length; i++) {
+				expect(myUtxoList[i].address).toBe(senderAddress);
 			}
 		});
 	});
