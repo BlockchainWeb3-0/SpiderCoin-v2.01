516d821a95008dbc8fffeea0a32e1b48cda36614
imysh578 <imysh578@gmail.com>
2022-02-06 16:53:02 +0900

    p2pServer.ts 1. move message class and messageType enum into message.ts

---
+++ server/server_p2p/message.ts
@@ -0,0 +1,40 @@
+import GlobalVar from "../blockchain/globalVar";
+
+enum MessageType {
+	QUERY_LAST_BLOCK,
+	QUERY_ALL_BLOCK,
+	RESPONSE_BLOCKCHAIN,
+	QUERY_TRANSACTION_POOL,
+	RESPONSE_TRANSACTION_POOL,
+}
+
+class Message {
+	public type: MessageType;
+	public data: any;
+	constructor(type: MessageType, data: any) {
+		this.type = type;
+		this.data = data;
+	}
+
+	static queryLastBlock = (): Message => ({
+		type: MessageType.QUERY_LAST_BLOCK,
+		data: null,
+	});
+	
+	static queryAllBlocks = (): Message => ({
+		type: MessageType.QUERY_ALL_BLOCK,
+		data: null,
+	});
+	
+	static responseLastBlock = (): Message => ({
+		type: MessageType.RESPONSE_BLOCKCHAIN,
+		data: JSON.stringify(GlobalVar.blockchain.getLastBlock()),
+	});
+	
+	static responseAllBlocks = (): Message => ({
+		type: MessageType.RESPONSE_BLOCKCHAIN,
+		data: JSON.stringify(GlobalVar.blockchain.blocks),
+	});
+}
+
+export {Message, MessageType}
